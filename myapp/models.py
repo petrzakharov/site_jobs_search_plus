1487f9d3f8c035410c9186441bf9c57396061a44
Petr Zakharov <zakharovp11@gmail.com>
2022-04-18 18:24:02 +0300

    add django models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.forms import ChoiceField, DateTimeField, URLField
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    logo = models.URLField(default='https://place-hold.it/100x60')
+    description = models.TextField(max_length=1000)
+    employee_count = models.IntegerField(
+        validators=[MinValueValidator(1)], blank=False
+    )
+
+
+class Specialty(models.Model):
+    code = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    picture = models.URLField(default='https://place-hold.it/100x60')
+
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=200)
+    specialty = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='vacancies'
+    )
+    skills = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    salary_min = models.FloatField(
+        validators=[MinValueValidator(1.0)], blank=False
+    )
+    salary_max = models.FloatField()
+    published_at = DateTimeField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
 
-# Create your views here.
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена, упс!</h1>')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('<h1>Произошла ошибка на сервере, упс!</h1>')
